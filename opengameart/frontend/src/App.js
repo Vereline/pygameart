4480e18b3e08dc4ec2543f5777b1dc0718b707d8
Viktoryia Stanko <vstanko1998@gmail.com>
2018-06-26 12:23:20 +0300

    Apply App menu simple design

--- opengameart/frontend/src/App.js
+++ opengameart/frontend/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import AppHeader from './AppHeader'
 import AppFooter from './AppFooter'
+import AppMenu from "./AppMenu";
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
           <h1 className="App-title">PyGameArt</h1>
           <AppHeader />
         </header>
+        <AppMenu />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ opengameart/frontend/src/AppMenu.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import './AppMenu.css';
+
+class AppMenu extends Component {
+  render() {
+    return (
+      <div className="AppMenu">
+        <ul className="AppMenu">
+          <li className="AppMenu-listItem"><a href="#">My photos</a></li>
+          <li className="AppMenu-listItem"><a href="#">My music</a></li>
+          <li className="AppMenu-listItem"><a href="#">My friends</a></li>
+          <li className="AppMenu-listItem"><a href="#">Popular Today</a></li>
+          <li className="AppMenu-listItem"><a href="#">Latest news</a></li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default AppMenu;
