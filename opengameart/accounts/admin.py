18f997c0493f14ee0200da77b8d4b68ed6220008
Viktoryia Stanko <vstanko1998@gmail.com>
2018-07-10 14:43:50 +0300

    Enable avatars for art users
    * + make margin and padding

--- opengameart/accounts/admin.py
+++ opengameart/accounts/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 from .models import ArtUser
 # Register your models here.
+from django.utils.html import mark_safe
 
 
 class ArtUserAdmin(admin.ModelAdmin):
     model = ArtUser
     list_display = ('user_id', 'location', 'art_direction')
-    fields = ('__str__', 'user_id', 'location', 'art_direction', 'description')
-    readonly_fields = ['__str__']
+    fields = ('__str__', 'user_id', 'user_avatar', 'display_image_field',  'location', 'art_direction', 'description')
+    readonly_fields = ['__str__', 'display_image_field']
+
+    def display_image_field(self, obj):
+        url = obj.user_avatar.url
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url=url,
+            width=300,
+            height=300,
+        )
+        )
 
 
 admin.site.register(ArtUser, ArtUserAdmin)
--- opengameart/accounts/models.py
+++ opengameart/accounts/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
 
 # Create your models here.
+image_storage = FileSystemStorage(
+    # Physical file location ROOT
+    location='{0}/images/'.format(settings.MEDIA_ROOT),
+    # Url for file
+    base_url='{0}images/'.format(settings.MEDIA_URL),
+)
+
+
+def image_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/images/avatars/<filename>
+    return 'avatars/{0}'.format(filename)
 
 
 class ArtUser(models.Model):
@@ -20,8 +33,37 @@ class ArtUser(models.Model):
     location = models.CharField(max_length=255, default="", null=True)
     description = models.TextField()
     art_direction = models.CharField(max_length=255, choices=ART_DIRECTION, default=art)
+    user_avatar = models.ImageField(upload_to=image_directory_path, storage=image_storage, null=True, blank=True)
 
     def __str__(self):
         """A string representation of the model."""
         current_user = User.objects.get(id=int(self.user_id))
         return current_user.username
+
+    def save(self, *args, **kwargs):
+        """ Save Art model."""
+        file_name = self.user_avatar.name
+        try:
+            art_user_id = get_id_by_path(file_name)
+            file_name = file_name.split('.')
+            # Rename image file into unique value
+            self.file_id = art_user_id
+            self.user_avatar.name = self.file_id + '.' + file_name[-1]
+
+        except Exception as e:
+            print("Error trying to save model: saving image failed: " + str(e))
+            pass
+
+        super(ArtUser, self).save(*args, **kwargs)
+
+
+def get_id_by_path(file_path):
+    import hashlib
+    import datetime
+    str_to_encode = file_path + str(datetime.datetime.now().date())
+    art_id = None
+    try:
+        art_id = hashlib.md5(str_to_encode.encode()).hexdigest()
+    except Exception as e:
+        print('{}, {}'.format(type(e), e))
+    return art_id
