6953c640c30b05dd69fcfc0dd6bbed3c7b9197f5
Viktoryia Stanko <vstanko1998@gmail.com>
2018-07-17 14:25:36 +0300

    Enable empty contacts and news pages

--- opengameart/arts/views.py
+++ opengameart/arts/views.py
@@ -113,3 +113,11 @@ class LoadGallery(generics.ListAPIView):
 
 def about_page(request):
     return render(request, 'about.html')
+
+
+def contacts_page(request):
+    return render(request, 'contacts.html')
+
+
+def news_page(request):
+    return render(request, 'news.html')
