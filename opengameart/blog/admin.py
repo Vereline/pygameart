2f0e9ed970a9b4eafbb51f9b04eb36ab85a6be96
Viktoryia Stanko <vstanko1998@gmail.com>
2018-07-25 13:39:39 +0300

    Enable images in news posts

--- opengameart/blog/admin.py
+++ opengameart/blog/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
+from opengameart.settings import STATIC_URL
 from .models import Post, Comment
 # Register your models here.
 
@@ -6,6 +9,20 @@ from .models import Post, Comment
 class PostAdmin(admin.ModelAdmin):
     model = Post
     list_display = ('author', 'title', 'created_date', 'published_date')
+    fields = ('author', 'title', 'created_date', 'published_date', 'text', 'post_image', 'display_image_field')
+    readonly_fields = ['display_image_field']
+
+    def display_image_field(self, obj):
+        if obj.post_image:
+            url = obj.post_image.url
+        else:
+            url = STATIC_URL + 'user-default.png'
+        return mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+            url=url,
+            width=300,
+            height=300,
+        )
+        )
 
 
 class CommentAdmin(admin.ModelAdmin):
--- opengameart/blog/models.py
+++ opengameart/blog/models.py
@@ -1,16 +1,31 @@
+from django.core.files.storage import FileSystemStorage
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
+
 # Create your models here.
+
+# Create your models here.
+image_storage = FileSystemStorage(
+    # Physical file location ROOT
+    location='{0}/images/'.format(settings.MEDIA_ROOT),
+    # Url for file
+    base_url='{0}images/'.format(settings.MEDIA_URL),
+)
+
+
+def image_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/images/avatars/<filename>
+    return 'news/{0}'.format(filename)
 
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_date = models.DateTimeField(
-            default=timezone.now)
-    published_date = models.DateTimeField(
-            blank=True, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    post_image = models.ImageField(upload_to=image_directory_path, storage=image_storage, null=True, blank=True)
 
     def publish(self):
         self.published_date = timezone.now()
