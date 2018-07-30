db5dcaa676ed4cf05b1a1fdd470e0d6e524247b5
Viktoryia Stanko <vstanko1998@gmail.com>
2018-07-30 14:57:12 +0300

    Implement method for resizing images and set in posts html image zoom function

--- opengameart/static/js/app.js
+++ opengameart/static/js/app.js
@@ -1,3 +1,31 @@
+function defineImageSize(pictWidth, pictHeight, maxWidth, maxHeight) {
+    let params = {
+        'width': 0,
+        'height': 0
+    };
+
+    if (pictHeight > pictWidth) {
+        let coefficient = pictHeight / maxHeight;
+        params['height'] = maxHeight;
+        params['width'] = pictWidth / coefficient;
+    } else if (pictHeight < pictWidth) {
+        let coefficient = pictWidth / maxWidth;
+        params['height'] = pictHeight/coefficient;
+        params['width'] = maxWidth;
+    } else {
+        let coefficient = 1;
+        if (maxWidth > maxHeight){
+            coefficient = pictHeight / maxHeight;
+        } else {
+            coefficient = pictWidth / maxWidth;
+        }
+        params['height'] = pictHeight / coefficient;
+        params['width'] = pictWidth/ coefficient;
+    }
+
+    return params
+}
+
 let bar = document.querySelector("#progress");
 window.addEventListener("scroll", () => {
 let max = document.body.scrollHeight - innerHeight;
